3d154c1c138926a9fc9fe2ece74e6fd3e43ff17a
ParamJyot01 <paramjyo483@gmail.com>
2026-08-23 23:49:36 +0530

    feat: connect navbar buttons to login/register routes using Link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom'
+
 function Navbar() {
   return (
     <nav className="navbar">
       <h2 className="navbar-logo">SpendWise</h2>
       <div className="navbar-links">
-        <button>Login</button>
-        <button>Sign Up</button>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/register">
+          <button>Sign Up</button>
+        </Link>
       </div>
     </nav>
   )
